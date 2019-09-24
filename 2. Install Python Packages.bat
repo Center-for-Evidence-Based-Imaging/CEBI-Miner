4eb2631eb36f8dcb2d39265f485c581573a38354
Raeen Hashemi <hashemi.s@husky.neu.edu>
2019-09-24 10:29:48 -0400

    Update 2. Install Python Packages.bat

--- 2. Install Python Packages.bat
+++ 2. Install Python Packages.bat
@@ -3,30 +3,30 @@ cd /d %~dp0
 for /f "delims=" %%a in ('pip3 -V') do @set version=%%a
 IF "%version:~0,3%" == "pip" (
 py -m pip install --user --upgrade pip
-pip3 install numpy
-pip3 install pandas
-pip3 install xlrd
-pip3 install openpyxl
-pip3 install xlwt
-pip3 install xlsxwriter
-pip3 install dash==0.28.1
-pip3 install dash-renderer==0.14.1
-pip3 install dash-core-components==0.33.0
-pip3 install dash-html-components==0.13.2
-pip3 install dash-table-experiments==0.6.0
+py -m pip install numpy
+py -m pip install pandas
+py -m pip install xlrd
+py -m pip install openpyxl
+py -m pip install xlwt
+py -m pip install xlsxwriter
+py -m pip install dash==0.28.1
+py -m pip install dash-renderer==0.14.1
+py -m pip install dash-core-components==0.33.0
+py -m pip install dash-html-components==0.13.2
+py -m pip install dash-table-experiments==0.6.0
 py -V
 ) ELSE (
 python -m pip install --user --upgrade pip
-pip install numpy
-pip install pandas
-pip install xlrd
-pip install openpyxl
-pip install xlwt
-pip install xlsxwriter
-pip install dash==0.28.1
-pip install dash-renderer==0.14.1
-pip install dash-core-components==0.33.0
-pip install dash-html-components==0.13.2
-pip install dash-table-experiments==0.6.0
+python -m pip install numpy
+python -m pip install pandas
+python -m pip install xlrd
+python -m pip install openpyxl
+python -m pip install xlwt
+python -m pip install xlsxwriter
+python -m pip install dash==0.28.1
+python -m pip install dash-renderer==0.14.1
+python -m pip install dash-core-components==0.33.0
+python -m pip install dash-html-components==0.13.2
+python -m pip install dash-table-experiments==0.6.0
 python -V
 )
